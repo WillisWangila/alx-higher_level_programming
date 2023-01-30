679b29f3be621a7b3a17d221a9e03fa196454e52
Wangila <wwangila@krw.localdomain>
2023-01-30 23:51:19 +0300

    Requirement fix

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -110,6 +110,22 @@ class Rectangle:
         else:
             i = 0
             while i < self.__height:
-                char_rectangle += (("#" * self.__width) + "\n")
+                char_rectangle += ("#" * self.__width)
+                if i < self.__height -1:
+                    char_rectangle += "\n"
                 i += 1
             return char_rectangle
+
+
+my_rectangle = Rectangle(2, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+print(str(my_rectangle))
+print(repr(my_rectangle))
+
+print("--")
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle)
+print(repr(my_rectangle))
