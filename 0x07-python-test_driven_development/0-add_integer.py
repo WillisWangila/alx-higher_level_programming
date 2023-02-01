4eb7cad4848c9aecc3d672f449209aa4d36606e6
Wangila <wwangila@krw.local>
2023-02-01 13:34:19 +0300

    Initial commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """
+    Checks type of variables and gets the sum of a and b
+
+    Casts float to int before calculating the sum
+
+    Args:
+        a (int or float): !st variable
+        b (int, or float optional): 2nd variable. Defaults to 98.
+
+    Raises:
+        TypeError: Raised if a is not an int or float
+
+    Returns:
+        int: sum of a and b
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+
+    return (int(a) + int(b))
