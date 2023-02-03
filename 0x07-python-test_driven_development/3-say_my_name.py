8582dc6a9595704e90f376d1989512499f276453
Wangila <wwangila@krw.local>
2023-02-03 22:21:42 +0300

    Tests

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""Defines a function that prints out the provided names"""
+
 
 def say_my_name(first_name, last_name=""):
+    """Prints 'My name is' followed by first name and optional last name
+
+    Args:
+        first_name (str): first name variable
+        last_name (str, optional): last name variable. Defaults to "".
+
+    Raises:
+        TypeError: _Raised if first_name is not a string
+        TypeError: Raised if last_name is not a string
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
