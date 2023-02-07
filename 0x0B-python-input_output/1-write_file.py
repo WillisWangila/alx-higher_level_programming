fbbcfcb328e42364b6ddc62edcdd46920382ef1b
Wangila <wwangila@krw.local>
2023-02-07 13:08:20 +0300

    Documentation fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,6 +6,15 @@ Write string to text file, return no of charaters written
 
 
 def write_file(filename="", text=""):
+    """Writes text to file
+
+    Args:
+        filename (str, optional): File to be written to. Defaults to "".
+        text (str, optional): Text written to file. Defaults to "".
+
+    Returns:
+        int: No. of characters written
+    """
     with open(filename, "w") as openfile:
         openfile.write(text)
         count = len(text)
