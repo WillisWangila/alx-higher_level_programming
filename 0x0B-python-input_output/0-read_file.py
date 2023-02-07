e6711f8915056086b317e82a6ff52f26978a8dfe
Wangila <wwangila@krw.local>
2023-02-07 12:53:26 +0300

    Initial commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Reads a text file and prints to stdout
+"""
+def read_file(filename=""):
+    """Opens file and prints content
+
+    Args:
+        filename (str, optional): Filename. Defaults to "".
+    """
+    with open(filename, encoding="utf-8") as openfile:
+        print(openfile.read())
