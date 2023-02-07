81a1e37769b9731314f68d1d6b5ca0fad6c0ec55
Wangila <wwangila@krw.local>
2023-02-07 17:05:15 +0300

    Initial commit

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Defines a function that creates an object from JSON file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    creates an object from JSON file
+    Args:
+        filename (json): file used to create the object
+    """
+    with open(filename, "r") as openfile:
+        json.load(openfile)
