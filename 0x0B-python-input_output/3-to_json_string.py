68e81a47cb64ed3af19e2cdeb7cab39fa33b5b37
Wangila <wwangila@krw.local>
2023-02-07 16:16:33 +0300

    Initial commit

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import json
+
+"""
+Returns the JSON representation of an object (string
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Returns json representation of a str object
+    """
+    return json.dumps(my_obj)
