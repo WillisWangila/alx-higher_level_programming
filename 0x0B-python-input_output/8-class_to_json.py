edcc25fd65eeed2c55a69d9ef0cb2c85dbb987f0
Wangila <wwangila@krw.localdomain>
2023-02-07 21:43:14 +0300

    Initial commit

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Defines a function that returns dictionary description with
+simple data structure for JSON serialization
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+    Returns dictionary description for JSON serialization
+    """
+    return json.dumps(obj.__dict__)
