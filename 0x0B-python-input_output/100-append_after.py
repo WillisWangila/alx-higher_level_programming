4b04c6cf5d2fc7b791370d6c66403ba78925002b
Wangila <wwangila@krw.localdomain>
2023-02-08 02:46:46 +0300

    Documentation fix

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -17,12 +17,9 @@ def append_after(filename="", search_string="", new_string=""):
     """
     doc = ""
     with open(filename, 'r') as read_file:
-            for line in read_file:
-                doc += line
-                if search_string in line:
-                    doc += new_string
-    with open (filename, 'w') as write_file:
+        for line in read_file:
+            doc += line
+            if search_string in line:
+                doc += new_string
+    with open(filename, 'w') as write_file:
         write_file.write(doc)
-
-
-append_after("sam", "Python", "\"C is fun!\"\n")
