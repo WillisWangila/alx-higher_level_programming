9dffda157e5d81705d8eace459c81bccf9a325e6
WillisWangila <w.wangila@gmail.com>
2023-09-12 00:56:15 +0300

    Multi languages initial commit

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const funC = 'C is fun';
+const coolPython = 'Python is cool';
+const amazingJS = 'JavaScript is amazing';
+console.log(funC);
+console.log(coolPython);
+console.log(amazingJS);
