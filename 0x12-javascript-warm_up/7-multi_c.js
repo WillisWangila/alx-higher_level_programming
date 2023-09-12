648d4f0a8fbd460a2b0158986d2fc7f845f3607e
WillisWangila <w.wangila@gmail.com>
2023-09-12 04:11:48 +0300

    Print lines of C is fun

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+if (isNaN(Number(args[0]))) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < Number(args[0]); i++) {
+    console.log('C is fun');
+  }
+}
