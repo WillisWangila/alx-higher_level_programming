79db88fba9cacbd64a547edd8cae12e569e867ff
WillisWangila <w.wangila@gmail.com>
2023-09-12 03:11:13 +0300

    Standards fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,8 @@
 
 const process = require('process');
 const args = process.argv.slice(2);
-let num = ''
-if (num = Math.trunc(args[0])) {
+let num = '';
+if ((num = Math.trunc(args[0]))) {
   console.log('My number: ', num);
 } else {
   console.log('Not a number');
