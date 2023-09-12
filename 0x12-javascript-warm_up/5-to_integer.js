2a63f828be0b53e6fe31df15a48475261463f1d9
WillisWangila <w.wangila@gmail.com>
2023-09-12 03:34:52 +0300

    more fixes

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const { argv } = require('process');
-const num = Math.floor(argv[2]);
+const num = Number(argv[2]);
 if (isNaN(num)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ', num);
+  console.log('My number: ', parseInt(num));
 }
