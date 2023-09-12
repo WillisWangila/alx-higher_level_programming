3582d1109821e837777154da75072130ba969279
WillisWangila <w.wangila@gmail.com>
2023-09-12 05:22:33 +0300

    sort and find 2nd largest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+if (args.length < 2) {
+  console.log(0);
+} else {
+    let sorted = (args.sort((a, b) => a - b));
+    console.log(sorted[sorted.length - 2]);
+}
