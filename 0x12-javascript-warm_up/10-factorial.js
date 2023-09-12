982fcd7c692c6de839ca657736775809144cdaa8
WillisWangila <w.wangila@gmail.com>
2023-09-12 05:07:03 +0300

    factorial fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-num = Number(args[0]);
+const num = Number(args[0]);
 function factorial (n) {
   if (n === 0 || isNaN(n)) return 1;
   return n * factorial(n - 1);
