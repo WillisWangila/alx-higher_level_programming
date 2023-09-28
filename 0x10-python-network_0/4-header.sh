354a1a5c5ee8829030ec205230ceec960c4094f6
WillisWangila <w.wangila@gmail.com>
2023-09-29 01:09:14 +0300

    Task 4 fix

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Accepts url, sends a GET request with custom header and displays body of response, uses curl
-curl -s -H "X-School-User-Id=98" "$1"
+curl -s -H "X-School-User-Id: 98" "$1"
