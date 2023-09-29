7227c5cfc5d3824ee6af52ae82a90145f1d44243
WillisWangila <w.wangila@gmail.com>
2023-09-29 03:35:30 +0300

    Fix task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Makes a request to sanndbox to make it respond with "You got me", use curl
-curl -s --location -X PUT -H "Origin: School" -d "user_id=98"  http://3cc921150c38.06a715d8.alx-cod.online:5000/catch_me
+curl -s --location -X PUT -H "Origin: School" -d "user_id=98"  0.0.0.0:5000/catch_me
