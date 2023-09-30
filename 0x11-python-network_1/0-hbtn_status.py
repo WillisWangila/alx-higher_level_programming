92aa56374ddb1965451034dbf8dd5cfb42a7606e
WillisWangila <w.wangila@gmail.com>
2023-09-30 05:00:55 +0300

    Typo fix task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ from urllib import request
 if __name__ == "__main__":
     with request.urlopen("https://intranet.hbtn.io/status") as body:
         body = body.read()
-    print("Body Response:")
+    print("Body response:")
     print(f"\t- type: {type(body)}")
     print(f"\t- content: {body}")
     print(f"\t- utf8 content: {body.decode('utf-8')}")
