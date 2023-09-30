e24f19f4469d5aea29c6def32c058ca6f2536663
WillisWangila <w.wangila@gmail.com>
2023-09-30 04:58:53 +0300

    Initial commit task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Uses urllib to fetch a page"""
+from urllib import request
+
+if __name__ == "__main__":
+    with request.urlopen("https://intranet.hbtn.io/status") as body:
+        body = body.read()
+    print("Body Response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode('utf-8')}")
