429a46531258d32cdbe684d048626ccf0349b781
WillisWangila <w.wangila@gmail.com>
2023-10-04 00:13:48 +0300

    Initial commit

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+script that lists the 10 most
+recent commits on a given GitHub repository
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    req = requests.get(url)
+    commits = req.json()
+
+    for commit in commits[:10]:
+        print(commit.get("sha"), end=": ")
+        print(commit.get('commit').get('author').get('name'))
